4f4f07361d13a8d55a1df573590a2637b821847d
Arunika123594 <arunika.n2024csbs@sece.ac.in>
2025-12-19 12:19:29 +0530

    change

--- Db/db.js
+++ Db/db.js
@@ -4,16 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const connectDb = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log("Database has been connected ✅");
-  } catch (err) {
-    console.error("Database connection error ❌:", err);
-    process.exit(1); // Exit the process if DB connection fails
-  }
+   try {
+      mongoose.connect(process.env.MONGODB_URL)
+      console.log("Database has been connected ✅");
+   } catch (err) {
+      console.error("Database connection error ❌:", err);
+      process.exit(1); // Exit the process if DB connection fails
+   }
 };
 
 export default connectDb;
